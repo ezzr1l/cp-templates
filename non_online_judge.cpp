e6bf0fff26e721b9cec225fad7b117ae8d1d14d7
Ilya <30858621+ezzr1l@users.noreply.github.com>
2021-08-04 20:59:19 +0300

    Add files via upload

---
+++ non_online_judge.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.141592653589793238462
+#define MOD1 998244353
+#define MOD 1000000007
+#define INF 1e18
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define set_bits __builtin_popcountll
+#define nline "\n"
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+int main() 
+{
+    fastio();
+
+    return 0;
+}
